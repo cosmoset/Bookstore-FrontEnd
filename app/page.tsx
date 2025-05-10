09e04f51c525f50a9ddda8f81435079af31fca6a
Nebiyou-x <nebiyoutad@gmail.com>
2025-05-10 23:53:30 +0300

    Added fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { LandingHero } from "@/pages/welcome";
-import { TestimonialsSectionDemo } from "@/All Components /testimony";
+import { TestimonialsSectionDemo } from "@/AllComponents /testimony";
 import  LandingPageClient  from "@/pages/landing"
-import NavHeader from "@/All Components /nav-header"
-import { Footer7 } from "@/All Components /footer";
+import NavHeader from "@/AllComponents /nav-header"
+import { Footer7 } from "@/AllComponents /footer";
 import InteractiveCheckoutDemo from "@/app/cart/page";
-import { Product } from "@/types/ptypes";
-import ProductListSec from "@/components/common/ProductList";
+import { Product } from "@/types/product.types";
+import ProductListSec from "@/components/common/ProductListSec";
+import { Review } from "@/types/review.types";
+
 
 
 
@@ -162,10 +164,56 @@ export const relatedProductData: Product[] = [
   },
 ];
 
+export const reviewsData: Review[] = [
+  {
+    id: 1,
+    user: "Alex K.",
+    content:
+      '"Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.”',
+    rating: 5,
+    date: "August 14, 2023",
+  },
+  {
+    id: 2,
+    user: "Sarah M.",
+    content: `"I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.”`,
+    rating: 5,
+    date: "August 15, 2023",
+  },
+  {
+    id: 3,
+    user: "Ethan R.",
+    content: `"This t-shirt is a must-have for anyone who appreciates good design. The minimalistic yet stylish pattern caught my eye, and the fit is perfect. I can see the designer's touch in every aspect of this shirt."`,
+    rating: 5,
+    date: "August 16, 2023",
+  },
+  {
+    id: 4,
+    user: "Olivia P.",
+    content: `"As a UI/UX enthusiast, I value simplicity and functionality. This t-shirt not only represents those principles but also feels great to wear. It's evident that the designer poured their creativity into making this t-shirt stand out."`,
+    rating: 5,
+    date: "August 17, 2023",
+  },
+  {
+    id: 5,
+    user: "Liam K.",
+    content: `"This t-shirt is a fusion of comfort and creativity. The fabric is soft, and the design speaks volumes about the designer's skill. It's like wearing a piece of art that reflects my passion for both design and fashion."`,
+    rating: 5,
+    date: "August 18, 2023",
+  },
+  {
+    id: 6,
+    user: "Samantha D.",
+    content: `"I absolutely love this t-shirt! The design is unique and the fabric feels so comfortable. As a fellow designer, I appreciate the attention to detail. It's become my favorite go-to shirt."`,
+    rating: 5,
+    date: "August 19, 2023",
+  },
+];
+
 export default async function Home() {
   return (
   <LandingPageClient>
-    <NavHeader/>
+    
     <LandingHero/>
     <div className="mt-16 md:mt-24">
       <ProductListSec
@@ -175,7 +223,6 @@ export default async function Home() {
       />
     </div>
     <TestimonialsSectionDemo/>
-    <Footer7/>
-  </LandingPageClient>
+   </LandingPageClient>
     );
 }
