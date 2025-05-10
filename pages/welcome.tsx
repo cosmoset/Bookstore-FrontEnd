52892f85c706e06729f46060be4fe2665dcaa61d
Nebiyou-x <nebiyoutad@gmail.com>
2025-05-11 00:37:35 +0300

    Resnavbar

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useRef } from "react"
+
 import Link from "next/link"
 import { LayoutGroup, motion } from "framer-motion"
 import { TextRotate } from "../AllComponents /uis/checkout/text-rotate"
